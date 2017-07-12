68bb2d39e38ddd6c407f019c367dda82c91fa1d7
MarionFoto <marionacp@gmail.com>
2017-07-12 01:02:03 -0400

    al apretar el icono cambia a foto 2 y 3

--- assets/main.js
+++ assets/main.js
@@ -1,7 +1,15 @@
 $(document).ready(function(){
-	$("#foto2").click(function(event) {
-		$("#agrega").append('<img src="assets/img/2.jpg" alt="">')
-	});
+		$("#foto2").click(function() {
+			$("#agrega").hide(function() {
+			$("#agrega2").append('<img src="assets/img/2.jpg" alt="">')	
+			});
+		});
+
+			$("#foto3").click(function() {
+			$("#agrega2").hide(function() {
+			$("#agrega3").append('<img src="assets/img/3.jpg" alt="">')	
+			});
+		});
 
 
 });
